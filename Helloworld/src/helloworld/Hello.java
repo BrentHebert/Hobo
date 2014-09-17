ada244b69491b9f9d8211995764747eed60fba04
BrentHebert <brent.hebert@colorado.edu>
2014-09-17 13:50:03 -0600

    Update Hello.java

--- Helloworld/src/helloworld/Hello.java
+++ Helloworld/src/helloworld/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 	System.out.println("Hello World!"); //Display the string.
 	System.out.println("How are you?");
+	// He's just fine!
 	}
 	}
 
